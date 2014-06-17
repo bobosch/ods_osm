b91b074d6232c3a0f5beef7e216d65791c8e4c23
Bobosch <Robert.Heel@gmx.de>
2014-06-17 19:08:28 +0200

    Compatibility to TYPO3 6.2

--- pi1/class.tx_odsosm_pi1.php
+++ pi1/class.tx_odsosm_pi1.php
@@ -27,7 +27,6 @@
  * Hint: use extdeveval to insert/update function index above.
  */
 
-require_once(PATH_tslib.'class.tslib_pibase.php');
 require_once(t3lib_extMgm::extPath('ods_osm').'class.tx_odsosm_common.php');
 require_once(t3lib_extMgm::extPath('ods_osm').'class.tx_odsosm_div.php');
 
