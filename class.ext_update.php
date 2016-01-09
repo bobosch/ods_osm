0995bb832d94f564f5a74e62ebb1206393e2ae7f
Robert Heel <Robert.Heel@gmx.de>
2016-01-09 21:22:01 +0100

    * Change: New version of update script.

--- class.ext_update.php
+++ class.ext_update.php
@@ -1,25 +1,100 @@
 <?php
-/**
- * Class for updating the db
- */
-class ext_update {
-	/**
-	 * Main function, returning the HTML content
-	 *
-	 * @return string HTML
-	 */
-	function main()	{
-		$content = '';
+/***************************************************************
+ *  Copyright notice
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
-		$objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
-		$databaseUpdateUtility = $objectManager->get('Bobosch\\OdsOsm\\Utility\\DatabaseUpdateUtility');
-		$status=$databaseUpdateUtility->doUpdate();
-		
-		if($status) return 'Update successful';
+class ext_update {
+	protected $messageArray = array();
+
+	public function access() {
+		return t3lib_div::compat_version('6.0');
 	}
 
-	function access() {
-		return t3lib_div::compat_version('6.0');
+	/**
+	 * Main update function called by the extension manager.
+	 *
+	 * @return string
+	 */
+	public function main() {
+		$this->processUpdates();
+		return $this->generateOutput();
+	}
+
+	/**
+	 * Generates output by using flash messages
+	 *
+	 * @return string
+	 */
+	protected function generateOutput() {
+		$output = '';
+		foreach ($this->messageArray as $messageItem) {
+			$flashMessage = t3lib_div::makeInstance(
+					't3lib_FlashMessage',
+					$messageItem[2],
+					$messageItem[1],
+					$messageItem[0]);
+			$output .= $flashMessage->render();
+		}
+
+		return $output;
+	}
+	
+	/**
+	 * The actual update function. Add your update task in here.
+	 *
+	 * @return void
+	 */
+	protected function processUpdates() {
+		$this->importStaticData();
+	}
+	
+	/**
+	 * Import static data
+	 *
+	 * @return int
+	 */
+	protected function importStaticData() {
+		$title = 'Import static data';
+		$message = '';
+		$status = NULL;
+
+		$extPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('ods_osm');
+		$fileContent = explode(LF, \TYPO3\CMS\Core\Utility\GeneralUtility::getUrl($extPath.'ext_tables_static+adt.sql'));
+		$GLOBALS['TYPO3_DB']->sql_query('TRUNCATE tx_odsosm_layer');
+		foreach ($fileContent as $line) {
+			$line = trim($line);
+			if ($line && preg_match('#^INSERT#i', $line)) {
+				if($GLOBALS['TYPO3_DB']->sql_query($line) === false) {
+					$message = ' SQL ERROR: ' .  $GLOBALS['TYPO3_DB']->sql_error();
+					$status = t3lib_FlashMessage::ERROR;
+				} else {
+					$message = 'OK!';
+					$status = t3lib_FlashMessage::OK;
+				}
+			}
+		}
+
+		$this->messageArray[] = array($status, $title, $message);
+		return $status;
 	}
 }
 ?>
