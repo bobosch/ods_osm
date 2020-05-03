c39c906e911756ab8a387f55af4a40c4e71d2a44
Alexander Bigga <alexander.bigga@slub-dresden.de>
2020-05-03 20:50:04 +0200

    Remove GeocodeWizard as func-module is deprecated.
    The func module is not available in TYPO3 9 anymore. It has been moved
    to a separate extensioni [1]. But his extension has not been developed
    anymore and does not work with TYPO3 9.5 LTS anymore.
    
    The GeocodeWizard itself uses the deprecated AbstractFunctionModule as
    mentioned in [#86225 - Classes BaseScriptClass and
    AbstractFunctionModule](https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/9.5/Deprecation-86225-ClassesBaseScriptClassAndAbstractFunctionModule.html).
    
    That's why I suggest to remove this feature.
    
    [1] https://github.com/FriendsOfTYPO3/func

--- ext_tables.php
+++ ext_tables.php
@@ -34,16 +34,3 @@ $iconRegistry->registerIcon(
 	\TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
 	[ 'source' => 'EXT:ods_osm/Resources/Public/Icons/ce_wiz.png' ]
 );
-
-if ( TYPO3_MODE == 'BE' ) {
-	if ( \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded( 'tt_address' ) ) {
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::insertModuleFunction(
-			'web_func',
-			\Bobosch\OdsOsm\FuncWizards\GeocodeWizard::class,
-			null,
-			'LLL:EXT:ods_osm/Resources/Private/Language/locallang.xml:wiz_geocode'
-		);
-	}
-}
-
-?>
