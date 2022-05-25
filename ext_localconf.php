6b814f0c22dcc4fc4ceca08ba5ddde40fb4c953d
Marcus Schwemer <marcus.schwemer@in2code.de>
2022-05-25 12:10:25 +0200

    [TASK] Re-add "old" TYPO3_MODE conditions
    The new syntax makes only sense, if we are on TYPO3 versions > 11
    
    Related: #83
    Signed-off-by: Marcus Schwemer <marcus.schwemer@in2code.de>

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,6 @@
 <?php
 
-defined('TYPO3') || die();
-
-use \TYPO3\CMS\Core\Http\ApplicationType;
-
+if (!defined('TYPO3_MODE')) die ('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43(
     'ods_osm',
@@ -13,8 +10,9 @@ use \TYPO3\CMS\Core\Http\ApplicationType;
     1
 );
 
-
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+if (TYPO3_MODE === 'BE') {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '
     mod.wizards.newContentElement.wizardItems.plugins.elements.odsosm {
         iconIdentifier = ods_osm_wizard
         title = LLL:EXT:ods_osm/Resources/Private/Language/locallang.xlf:pi1_title
@@ -26,11 +24,14 @@ use \TYPO3\CMS\Core\Http\ApplicationType;
     }
 
     mod.wizards.newContentElement.wizardItems.plugins.show := addToList(odsosm)
-    ');
+    '
+    );
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
+        '
         options.saveDocNew.tx_odsosm_track=1
-    ');
+    '
+    );
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \Bobosch\OdsOsm\TceMain::class;
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals'][\Bobosch\OdsOsm\Evaluation\LonLat::class] = '';
@@ -65,5 +66,5 @@ use \TYPO3\CMS\Core\Http\ApplicationType;
     }
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['odsOsmFileLocationUpdater']
-            = \Bobosch\OdsOsm\Updates\FileLocationUpdater::class;
-
+        = \Bobosch\OdsOsm\Updates\FileLocationUpdater::class;
+}
