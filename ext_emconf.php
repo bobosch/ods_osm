9e01f03f58781931af8212abc0c0c7809e944dbd
Alexander Bigga <alexander.bigga@slub-dresden.de>
2022-05-31 10:30:03 +0200

    Prepare version number for release

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,5 +15,5 @@ $EM_CONF[$_EXTKEY] = [
     'createDirs' => 'uploads/tx_odsosm/map',
     'state' => 'stable',
     'uploadfolder' => 1,
-    'version' => '4.0.0-dev',
+    'version' => '4.0.0',
 ];
