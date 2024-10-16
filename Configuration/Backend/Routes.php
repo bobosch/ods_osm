67ad091d2c781af0c883bfc019bfffbfe0344d18
Alexander Bigga <alexander@bigga.de>
2024-10-16 22:46:24 +0200

    Remove unused backend route

--- Configuration/Backend/Routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Bobosch\OdsOsm\Wizard\Coordinatepicker;
-return [
-    'coordinatepicker' => [
-        'path' => '/wizard/ods_osm/coordinatepicker',
-        'target' => Coordinatepicker::class . '::mainAction'
-    ],
-];
