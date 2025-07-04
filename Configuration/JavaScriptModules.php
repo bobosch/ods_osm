6f8a7f79b78f94219dbcaf13b34eac329b7ee270
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2025-07-04 11:16:19 +0200

    Fix JS integration in the BE for TYPO3 12.4
    Integration for TYPO3 13.4 still broken

---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Copyright notice
+ *
+ * (c) DMK E-BUSINESS GmbH <dev@dmk-ebusiness.de>
+ * All rights reserved
+ *
+ * This file is part of TYPO3 CMS-based extension "mkcontentai" by DMK E-BUSINESS GmbH.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ */
+
+return [
+    'dependencies' => ['core', 'backend'],
+    'imports' => [
+        '@bobosch/ods_osm/' => 'EXT:ods_osm/Resources/Public/JavaScript/',
+    ],
+];
